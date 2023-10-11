1193b34b2f84b78f36934e2da75d1a7aa5331c64
Joseph Larbi <lawsonjoey546@gmail.com>
2023-10-11 12:13:19 +0000

    Set up for my e shop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         You're Welcome To
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dreamhousechapelint.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Dream House Chapel Int.
         </a>
       </header>
     </div>
